6613b840270207c55a03ebf53045f7b92269cbb9
Ekramul <mdekramulhassan168@gmail.com>
2024-07-09 07:29:40 +0600

    global error handele added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import notFound from "./app/middlewares/notFound";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(notFound);
 
 export default app;

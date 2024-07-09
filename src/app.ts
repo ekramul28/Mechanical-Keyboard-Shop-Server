d08b030c2166405e6f428c87945ae12664152c8b
Ekramul <mdekramulhassan168@gmail.com>
2024-07-09 08:09:55 +0600

    database added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
-import express from "express";
+import express, { Application } from "express";
 import notFound from "./app/middlewares/notFound";
-const app = express();
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globalErrorhandler";
+const app: Application = express();
+
+//parsers
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server Running");
 });
+
+app.use(globalErrorHandler);
 
 app.use(notFound);
 

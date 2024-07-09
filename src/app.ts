3375ce676ae556d39d992b0317ece575f663d9c8
Ekramul <mdekramulhassan168@gmail.com>
2024-07-09 08:16:36 +0600

    router added

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express, { Application } from "express";
 import notFound from "./app/middlewares/notFound";
 import cors from "cors";
 import globalErrorHandler from "./app/middlewares/globalErrorhandler";
+import router from "./app/routes";
 const app: Application = express();
 
 //parsers
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/v1", router);
 
 app.get("/", (req, res) => {
   res.send("Server Running");
